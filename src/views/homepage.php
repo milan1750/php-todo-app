27b8c3a4f00753879acc61c81c5c4edb4cde834a
Milan Malla <milanmalla2015@gmail.com>
2023-05-11 15:19:53 +0100

    Task status added

--- src/views/homepage.php
+++ src/views/homepage.php
@@ -43,68 +43,89 @@
 				}
 			?>
 			<!-- ./add todo -->
+			<div class="row">
+				<div class="col-md-8">
+					<!-- todo table -->
+					<table class="table border rounded mt-5 table-striped todo-table">
+						<thead>
+							<tr>
+							<th scope="col">#</th>
+							<th class="text-center" scope="col">Tasks</th>
+							<th>Status</th>
+							<th class="text-center" scope="col">
+								Actions
+							</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php $sno = 1; foreach( $todos as $row ){?>
+								<tr>
+								<th scope="row" ><?php echo $sno++;?></th>
+								<td class="text-center">
+									<div class="task-list">
+										<?php echo $row['tasks'];?>
+									</div>
+								</td>
+								<td>
+										<?php
+										switch ($row['status']) {
+											case 'Complete':
+												$badge_color = 'success';
+												break;
+											case 'Todo':
+												$badge_color = 'primary';
+												break;
+											case 'In Progress':
+												$badge_color = 'info';
+												break;
+											default:
+												$badge_color = 'primary';
+												break;
+										}
+										?>
+									<?php echo '<span class="btn btn-'.$badge_color.'">'. ( ! empty($row['status']) ? $row['status'] : 'Pending' ) . '</span>'; ?>
+								</td>
+								<td >
+									<form action="?page=todo&action=status-todo&id=<?php echo $row['id'];?>" method="post" style="margin: 0px;padding: 0px;" class="list-form">
+										<!-- <?php if( 'Todo' !== $row['status'] ){ ?>
+											<button class="btn btn-primary" name="todo-todo-btn" id="todo-todo-btn" type="  submit">Todo</button>
+										<?php } ?>
+										<?php if( 'In Progress' !== $row['status'] ){ ?>
+											<button class="btn btn-info" name="inp-todo-btn" id="inp-todo-btn" type="  submit">In Progress</button>
+										<?php } ?>
+										<?php if( 'Complete' !== $row['status'] ){ ?>
+											<button class="btn btn-success" name="complete-todo-btn" id="complete-todo-btn" type="  submit">Complete</button>
+										<?php } ?> -->
+										<button class="btn btn-primary" name="view-todo-btn" id="view-todo-btn" type="submit">View</button>
+										<button class="btn btn-danger" name="dlt-todo-btn" id="dlt-todo-btn" type="submit">Delete</button>
+									</form>
+								</td>
+								</tr>
+							<?php }?>
 
-			<!-- todo table -->
-			<table class="table border rounded mt-5 table-striped todo-table">
-				<thead>
-					<tr>
-					<th scope="col">#</th>
-					<th class="text-center" scope="col">Tasks</th>
-					<th>Status</th>
-					<th class="text-center" scope="col">
-						Actions
-					</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php $sno = 1; foreach( $todos as $row ){?>
-						<tr>
-						<th scope="row" ><?php echo $sno++;?></th>
-						<td class="text-center">
-							<div class="task-list">
-								<?php echo $row['tasks'];?>
-							</div>
-						</td>
-						<td>
-								<?php
-								switch ($row['status']) {
-									case 'Complete':
-										$badge_color = 'success';
-										break;
-									case 'Todo':
-										$badge_color = 'primary';
-										break;
-									case 'In Progress':
-										$badge_color = 'info';
-										break;
-									default:
-										$badge_color = 'primary';
-										break;
-								}
-								?>
-							<?php echo '<span class="btn btn-'.$badge_color.'">'. ( ! empty($row['status']) ? $row['status'] : 'Pending' ) . '</span>'; ?>
-						</td>
-						<td >
-							<form action="?page=todo&action=status-todo&id=<?php echo $row['id'];?>" method="post" style="margin: 0px;padding: 0px;" class="list-form">
-								<!-- <?php if( 'Todo' !== $row['status'] ){ ?>
-									<button class="btn btn-primary" name="todo-todo-btn" id="todo-todo-btn" type="  submit">Todo</button>
-								<?php } ?>
-								<?php if( 'In Progress' !== $row['status'] ){ ?>
-									<button class="btn btn-info" name="inp-todo-btn" id="inp-todo-btn" type="  submit">In Progress</button>
-								<?php } ?>
-								<?php if( 'Complete' !== $row['status'] ){ ?>
-									<button class="btn btn-success" name="complete-todo-btn" id="complete-todo-btn" type="  submit">Complete</button>
-								<?php } ?> -->
-								<button class="btn btn-primary" name="view-todo-btn" id="view-todo-btn" type="submit">View</button>
-								<button class="btn btn-danger" name="dlt-todo-btn" id="dlt-todo-btn" type="submit">Delete</button>
-							</form>
-						</td>
-						</tr>
-					<?php }?>
+						</tbody>
+					</table>
+					<!-- ./todo table -->
+				</div>
+				<div class="col-md-4">
+					<h4>Completed Task</h4>
+					<ul>
+						<li>Code and Write validation rules</li>
+						<li>CICD pipeline integration</li>
+						<li>Login/Register page wireframe design completed</li>
+						<li>Home page wireframe design completed</li>
+					</ul>
+					<h4>Task in Progress</h4>
+					<ul>
+						<li>Finalizing report</li>
+						<li>QA testing for final version</li>
+						<li>Video Prepration</li>
+					</ul>
+				</div>
+			</div>
 
-				</tbody>
-			</table>
-			<!-- ./todo table -->
+
+
 		</div>
 	</div>
 </div>
