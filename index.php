f3dd500322e3ca4a532b1087bd66e8dca02db958
Milan Malla <milanmalla2015@gmail.com>
2023-05-11 15:04:21 +0100

    new layout added

--- index.php
+++ index.php
@@ -47,7 +47,57 @@ session_start();
 			$GLOBALS['todo'] = todo();
 			?>
 		</div>
+
+		<!-- Modal -->
+		<div class="modal fade" id="viewModal" tabindex="-1" role="dialog" aria-labelledby="viewModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="viewModalLabel">Project Work - Software Engineering</h5>
+			</div>
+			<div class="modal-body">
+				<div class="form-group mt-2">
+					<label for="">Description</label>
+					<textarea class="form-control" name="" id="" cols="30" rows="10">Need to submit project report before the deadline</textarea>
+				</div>
+				<div class="form-group mt-2">
+					<label for="">Status</label>
+					<select name="" id="" class="form-control">
+						<option value="">Todo</option>
+						<option value="">In Progress</option>
+						<option value="">Complete</option>
+					</select>
+				</div>
+				<div class="form-group mt-2">
+					<label for="">Deadline</label>
+					<input type="date" class="form-control" value="12/05/2023">
+				</div>
+				<div class="form-group mt-2">
+					<input type="checkbox">
+					<label for="">Enable Notification</label>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-primary">Save changes</button>
+			</div>
+			</div>
+		</div>
+		</div>
         <!-- ./Signup Form -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+
+		<script>
+			$(document).ready(function(){
+				$( "#add-btn" ).click(function() {
+						$("#add-todo-form").toggle();
+				});
+
+				$( '.list-form' ).on( 'click', '#view-todo-btn', function(e){
+					e.preventDefault();
+					$('#viewModal').modal('toggle');
+				})
+			});
+		</script>
     </body>
 </html>
--- src/views/homepage.php
+++ src/views/homepage.php
@@ -5,9 +5,9 @@
  ?>
 
 <!-- Navbar -->
-<nav class="navbar">
+<nav class="navbar bg-white p-4">
 	<div class="container">
-		<span class="navbar-brand mb-0 h1"><?php echo $user['username'];?></span>
+		<span class="navbar-brand mb-0 h1"><img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" class="profile-image">&nbsp;&nbsp;<?php echo $user['username'];?></span>
 		<div class="d-flex justify-content-end logout-btn"><a href="?page=logout"><img src="assets/images/logout.png" class="logout-image"></a></div>
 	</div>
 </nav>
@@ -24,8 +24,15 @@
 			<div id="add-todo-form" class="mt-5">
 				<form action="?page=todo&action=add-todo" method="post" >
 					<div class="input-group mb-3">
+						<label for="Task Title"></label>
 						<input type="text" class="form-control" id="add-todo" name="add-todo" placeholder="Enter your todo here"  >
-						<button class="btn btn-primary" name="add-todo-btn" id="add-todo-btn" type="submit">Add</button>
+					</div>
+					<div class="input-group mb-3">
+						<label for="Task Description"></label>
+						<input type="text" class="form-control" id="add-todo" name="add-todo" placeholder="Enter your todo here"  >
+					</div>
+					<div class="form-group">
+						<button class="btn btn-primary" name="add-todo-btn" id="add-todo-btn" type="submit">Add Task</button>
 					</div>
 				</form>
 			</div>
@@ -78,8 +85,8 @@
 							<?php echo '<span class="btn btn-'.$badge_color.'">'. ( ! empty($row['status']) ? $row['status'] : 'Pending' ) . '</span>'; ?>
 						</td>
 						<td >
-							<form action="?page=todo&action=status-todo&id=<?php echo $row['id'];?>" method="post" style="margin: 0px;padding: 0px;">
-									<?php if( 'Todo' !== $row['status'] ){ ?>
+							<form action="?page=todo&action=status-todo&id=<?php echo $row['id'];?>" method="post" style="margin: 0px;padding: 0px;" class="list-form">
+								<!-- <?php if( 'Todo' !== $row['status'] ){ ?>
 									<button class="btn btn-primary" name="todo-todo-btn" id="todo-todo-btn" type="  submit">Todo</button>
 								<?php } ?>
 								<?php if( 'In Progress' !== $row['status'] ){ ?>
@@ -87,7 +94,8 @@
 								<?php } ?>
 								<?php if( 'Complete' !== $row['status'] ){ ?>
 									<button class="btn btn-success" name="complete-todo-btn" id="complete-todo-btn" type="  submit">Complete</button>
-								<?php } ?>
+								<?php } ?> -->
+								<button class="btn btn-primary" name="view-todo-btn" id="view-todo-btn" type="submit">View</button>
 								<button class="btn btn-danger" name="dlt-todo-btn" id="dlt-todo-btn" type="submit">Delete</button>
 							</form>
 						</td>
@@ -100,11 +108,3 @@
 		</div>
 	</div>
 </div>
-
-<script>
-	$(document).ready(function(){
-		$( "#add-btn" ).click(function() {
-				$("#add-todo-form").toggle();
-		});
-	});
-</script>
